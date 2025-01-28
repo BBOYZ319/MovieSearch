1481362f9982d323d2b4c5024caae0cb5101c7a0
BBOYZ319 <boyfantastik18@gmail.com>
2025-01-28 19:55:39 +0700

    Delete movie-route.js

--- movie-route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const movieController = require('../controllers/user-controller');
-
-router.get('/popular-movies', movieController.getPopularMovies);
-
-module.exports = router;
